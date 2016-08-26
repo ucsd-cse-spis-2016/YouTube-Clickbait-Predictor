ee39a12a566d2402b115ef782e1678052f1cd4ef
Ryan L <r9le@ucsd.edu>
2016-08-26 09:22:25 -0700

    defined funcs

--- youtubetitles.py
+++ youtubetitles.py
@@ -2,14 +2,20 @@ import json
 import requests
 import pprint #make sure to do pprint.pprint("string") when printing
 
-
+#https://www.googleapis.com/youtube/v3/search?part=snippet&key=[API_KEY]
 #After getting stuff from API, turns into code and stuff
-result = requests.get("insert link here")
 
-#makes into a dict
-rdata = json.loads(result.text)
+def getVids(link):
+        result = requests.get(link)
+        return result
 
-#how to print the titles
-for i in range(totalResults):
-	pprint.pprint(rdata['items'][i]['snippet']['title'])
+def makeDict(result):
+        #makes into a dict
+        rdata = json.loads(result.text)
+        return rdata
+
+def printAllTitles(rdata):
+        #how to print the titles
+        for i in range(50):
+                pprint.pprint(rdata['items'][i]['snippet']['title'])
 
